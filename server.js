20490299b8d6f42696bfd680552ef68e3bd6f01e
Jorge Pezzuol <jorge_pezzuol@hotmail.com>
2020-04-13 17:46:42 +0100

    the link of images from food/restaurants are not sent anymore

--- server.js
+++ server.js
@@ -6,4 +6,6 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api', require('./src/routes'));
+app.use('/resources', express.static(__dirname + '/images'));
+
 app.listen(3005);
--- src/controllers/iFoodController.js
+++ src/controllers/iFoodController.js
@@ -1,25 +1,20 @@
 const axios = require("axios").default;
 
 module.exports = {
+
 	async fetchFood(req, res) {
+	
 		// latitude=-23.53827&longitude=-46.22185&categories=
 		const apiUrl =
 			"https://marketplace.ifood.com.br/v1/merchants?latitude=-23.53827&longitude=-46.22185&zip_code=08790320&page=0&channel=IFOOD&size=10&sort=&categories=&payment_types=&delivery_fee_from=0&delivery_fee_to=25&delivery_time_from=0&delivery_time_to=90";
 
-        const promises = [];
-        const foodMenu = [];
+		const promises = [];
+		const foodMenu = [];
 
 		const response = await axios.get(apiUrl);
 		const data = await response.data;
 
 		for (d of data.merchants) {
-
-            let fileName = "";
-            let resources = d.resources[0];
-
-            if(resources.hasOwnProperty('fileName')) {
-                fileName = `https://static-images.ifood.com.br/image/upload/f_auto,t_thumbnail/logosgde/${resources.fileName}`;
-            }
 
 			const menuRequests = await axios
 				.get(
@@ -28,52 +23,39 @@ module.exports = {
 						headers: {
 							access_key: "69f181d5-0046-4221-b7b2-deef62bd60d5",
 							secret_key: "9ef4fb4f-7a1d-4e0d-a9b1-9b82873297d8"
-                        },
-                        params: {
-                            restaurant: {
-                                name: d.name,
-                                distance: d.distance,
-                                slug: d.slug,
-                                detailUrl: `https://www.ifood.com.br/delivery/${d.slug}/${d.id}`,
-                                fileName: fileName
-                            }
-                        }
+						},
+						params: {
+							restaurant: {
+								id: d.id,
+								name: d.name,
+								distance: d.distance,
+								slug: d.slug,
+								detailUrl: `https://www.ifood.com.br/delivery/${d.slug}/${d.id}`,
+								logo: d.resources[0]
+							}
+						}
 					}
 				)
 				.catch(e => {
-					console.log(e);
-					//res.end()
-                });
-                
+				});
+
 			promises.push(menuRequests);
 		}
 
 		await Promise.all(promises)
 			.then(results => {
-				
 				for (r of results) {
 					for (m of r.data.data.menu) {
 						for (i of m.itens) {
-
-                            restaurant = r.config.params.restaurant;
-
-							if(i.logoUrl != undefined) {
-							    i.logoUrl = `https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/${i.logoUrl}`;
-							}
-							else {
-							    i.logoUrl = restaurant.fileName;
-                            }
-                            i.restaurant = restaurant;
+							i.restaurant = r.config.params.restaurant;
 							foodMenu.push(i);
 						}
 					}
 				}
 			})
 			.catch(e => {
-                //Promise.reject(Error("oops"));
-                //console.log(e);
 			});
-        
-        return res.json(foodMenu);
+
+		return res.json(foodMenu);
 	}
 };
