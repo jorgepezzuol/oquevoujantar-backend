9fb3f2972d7771506b7d2305204111a3887193c1
Jorge Pezzuol <jorge_pezzuol@hotmail.com>
2020-04-14 01:38:05 +0100

    now filtering for categories

--- src/controllers/iFoodController.js
+++ src/controllers/iFoodController.js
@@ -3,10 +3,9 @@ const axios = require("axios").default;
 module.exports = {
 
 	async fetchFood(req, res) {
-	
-		// latitude=-23.53827&longitude=-46.22185&categories=
+
 		const apiUrl =
-			"https://marketplace.ifood.com.br/v1/merchants?latitude=-23.53827&longitude=-46.22185&zip_code=08790320&page=0&channel=IFOOD&size=10&sort=&categories=&payment_types=&delivery_fee_from=0&delivery_fee_to=25&delivery_time_from=0&delivery_time_to=90";
+			`https://marketplace.ifood.com.br/v1/merchants?categories=${req.query.category == undefined ? "" : req.query.category}&latitude=-23.53827&longitude=-46.22185&zip_code=08790320&page=0&channel=IFOOD&size=10&sort=&payment_types=&delivery_fee_from=0&delivery_fee_to=25&delivery_time_from=0&delivery_time_to=90`;
 
 		const promises = [];
 		const foodMenu = [];
