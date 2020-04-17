04564e5190b13f41d91edc6fb534f1adc1e08779
Jorge Pezzuol <jorge_pezzuol@hotmail.com>
2020-04-17 11:50:48 +0100

    trying to debug and see why not working on heroku

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ const routes = express.Router();
 const iFoodController = require('./controllers/iFoodController');
 
 routes.get("/fetchFood", iFoodController.fetchFood);
+routes.get("/test", (req, res) => {
+    alert("aa");
+});
 
 module.exports = routes;
