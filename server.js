5a8e702d30aaaab61f0e55b3612848406edebe2f
Jorge Pezzuol <jorge_pezzuol@hotmail.com>
2020-04-17 12:11:34 +0100

    changing listening port

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 
+require('dotenv').config();
+
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -8,5 +11,6 @@ app.use(cors());
 app.use('/api', require('./src/routes'));
 app.use('/resources', express.static(__dirname + '/images'));
 
+const NODE_PORT = 'process.env.PORT || 3000';
 //app.listen(3005);
-app.listen(8080);
+app.listen(NODE_PORT);
